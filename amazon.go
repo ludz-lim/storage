c4081e83e9f49ee42fc848a75814b57b705dd65d
Ludwig Isaac Lim <hou-fu.lin@hp.com>
2021-04-15 10:30:14 +0800

    Add logging

--- amazon.go
+++ amazon.go
@@ -41,6 +41,7 @@ type AmazonS3Backend struct {
 
 // NewAmazonS3Backend creates a new instance of AmazonS3Backend
 func NewAmazonS3Backend(bucket string, prefix string, region string, endpoint string, sse string) *AmazonS3Backend {
+        println("ludwig log ---> calling NewAmazonS3Backend()")
         awsConfig = aws.NewConfig()
         awsConfig.WithRegion(aws.String(region))
         if endpoint != "" && endpoint != nil {
@@ -62,6 +63,7 @@ func NewAmazonS3Backend(bucket string, prefix string, region string, endpoint st
 
 // NewAmazonS3BackendWithCredentials creates a new instance of AmazonS3Backend with credentials
 func NewAmazonS3BackendWithCredentials(bucket string, prefix string, region string, endpoint string, sse string, credentials *credentials.Credentials) *AmazonS3Backend {
+        println("ludwig log ---> calling NewAmazonS3BackendWithCredentials()")
 	service := s3.New(session.New(), &aws.Config{
 		Credentials:      credentials,
 		Region:           aws.String(region),
